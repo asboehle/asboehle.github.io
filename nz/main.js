7ae994d73131e99c7681fc13530dae45beb8a158
asboehle <Ann-Sophie.Boehle@student.uibk.ac.at>
2021-03-25 16:13:24 +0100

    Layern hinzugefügt

--- nz/main.js
+++ nz/main.js
@@ -1,6 +1,7 @@
 const map = L.map("map", {
  center: [ -44.616667, 167.866667 ],
- zoom: 13
+ zoom: 13,
+ layers: [ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png") ]
 }); 
 //ID erstellen; L für Leaflet, []stehen für Lsiten
 console.log(document.querySelector("#map"));//documentquerySelector wenn wir aus einem Scirpt auf eine Karte zugreifen wollen
