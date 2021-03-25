682be0aaf3e6f8dc8dc7b828a5dbffa5cdbee8f0
asboehle <Ann-Sophie.Boehle@student.uibk.ac.at>
2021-03-25 16:22:55 +0100

    Popup hinzugefügen

--- nz/main.js
+++ nz/main.js
@@ -4,7 +4,8 @@ const map = L.map("map", {
  layers: [ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png") ]
 }); 
 
-let mrk =  L.marker ([-44.616667, 167.866667]).addTo(map);
+let mrk =  L.marker ([-44.616667, 167.866667]).addTo(map); //marker definieren und karte hinzufügen
+mrk.bindPopup("Milford Sound").openPopup(); //beschriftet marke, Popup feature geht immer nur auf einem Marker
 
 //ID erstellen; L für Leaflet, []stehen für Lsiten
 console.log(document.querySelector("#map"));//documentquerySelector wenn wir aus einem Scirpt auf eine Karte zugreifen wollen
