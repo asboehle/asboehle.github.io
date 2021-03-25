6526c0c737dd8f5f9f0f7e171a9a10bd32abd10d
asboehle <Ann-Sophie.Boehle@student.uibk.ac.at>
2021-03-25 17:27:14 +0100

    Neu

--- nz/main.js
+++ nz/main.js
@@ -15,12 +15,13 @@ const map = L.map("map", {
  console.log(ROUTE)
  for (let entry of ROUTE) {
      console.log(entry)
- }
+ 
 let mrk =  L.marker ([entry.lat, entry.lng]).addTo(map); //marker definieren und karte hinzufügen
 mrk.bindPopup(`
     <h4>entry ${entry.nr}: ${entry.name}</h4>
     <p><i class="fas fa-external-link-alt mr-3"></i><a href="${entry.wikipedia}"> Read about stop in wikpipedia</a></p>
     `).openPopup();
+}
 
 //beschriftet marke, Popup feature geht immer nur auf einem Marker
 
