3d92473d295238920b1593b12691b6b8b323db67
asboehle <Ann-Sophie.Boehle@student.uibk.ac.at>
2021-03-25 16:07:44 +0100

    Zoomlevel definiert

--- nz/main.js
+++ nz/main.js
@@ -1,5 +1,6 @@
 const map = L.map("map", {
- center: [ -44.616667, 167.866667 ]
+ center: [ -44.616667, 167.866667 ],
+ zoom: 13
 }); 
 //ID erstellen; L für Leaflet, []stehen für Lsiten
 console.log(document.querySelector("#map"));//documentquerySelector wenn wir aus einem Scirpt auf eine Karte zugreifen wollen
