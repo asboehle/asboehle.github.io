1a11f455608ce2f96ff12b5294bcad3c7ba3340b
asboehle <Ann-Sophie.Boehle@student.uibk.ac.at>
2021-03-25 16:20:06 +0100

    marker hinzugefügt

--- nz/main.js
+++ nz/main.js
@@ -3,5 +3,10 @@ const map = L.map("map", {
  zoom: 13,
  layers: [ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png") ]
 }); 
+
+let mrk =  L.marker ([-44.616667, 167.866667]).addTo(mao);
+
 //ID erstellen; L für Leaflet, []stehen für Lsiten
 console.log(document.querySelector("#map"));//documentquerySelector wenn wir aus einem Scirpt auf eine Karte zugreifen wollen
+
+//cmd + i in Webseite um verwendete Medien anzusehen
