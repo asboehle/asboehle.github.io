333511e109a07cdeb3d9deddb522443de62b7dba
asboehle <Ann-Sophie.Boehle@student.uibk.ac.at>
2021-04-15 16:35:37 +0200

    WEtterdaten einbetten

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -1,14 +1,25 @@
-let basemapGrey = L.titleLayer.provider ()
+let basemapGray = L.tileLayer.provider('BasemapAT.grau');
 
-let map = L.map ("map"; {
-    cener: [24, 11],
+let map = L.map("map", {
+    center: [47, 11],
     zoom: 9,
     layers: [
-        basemapGrey
+        basemapGray
     ]
 });
-let layerControl = L.control.layers({
-    "BasemapAT.grau": basemapGrey,
-    "BasemapAT.ortofoto": L.titleLayer.provider('BasemapAT')
 
-}). addTo(map);
+let layerControl = L.control.layers({
+    "BasemapAT.grau": basemapGray,
+    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto')
+    "BasemapAT.surface": L.titleLayer.provider('BasemapAT.surface')
+    "BasemapAT.overlay": L.titleLayer.provider('BasemapAT.overlay')
+    "BasemapAT.overlay +ortho": L.layerGroup([
+        L.titleLayer.provider ('BasemapAT.orthofoto'),
+        L.titleLayer.provider ('BasemapAT.overlay')
+    ])
+}).addTo(map);
+
+let awsUrl = "https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson";
+fetch(awsUrl).then(response => {
+    console.log('Daten geladen', response.json());
+})
