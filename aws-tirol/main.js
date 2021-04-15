b0116502d4c58c1674ed0ceab4878448f8cad327
asboehle <Ann-Sophie.Boehle@student.uibk.ac.at>
2021-04-15 16:48:47 +0200

    marker hinzugefügt

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -11,15 +11,25 @@ let map = L.map("map", {
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray,
     "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto')
-    "BasemapAT.surface": L.titleLayer.provider('BasemapAT.surface')
-    "BasemapAT.overlay": L.titleLayer.provider('BasemapAT.overlay')
-    "BasemapAT.overlay +ortho": L.layerGroup([
-        L.titleLayer.provider ('BasemapAT.orthofoto'),
-        L.titleLayer.provider ('BasemapAT.overlay')
+    "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
+    "BasemapAT.overlay+ortho": L.layerGroup([
+        L.tileLayer.provider('BasemapAT.orthofoto'),
+        L.tileLayer.provider('BasemapAT.overlay')
     ])
 }).addTo(map);
 
 let awsUrl = "https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson";
-fetch(awsUrl).then(response => {
-    console.log('Daten geladen', response.json());
-})
+
+fetch(awsUrl)
+    .then(response => resoponse.json())
+    .then(json => {
+        console.log('Daten Konvertiert: ', json);
+        for (station of json.features) {
+        console.log('Satation: ', station);
+        let marker = L.marker(
+            [station.geometry.coodrinates[1],
+            station.geometry.coodrinates[0]]
+        );
+        marker.addTo(map);
+    }
+});
