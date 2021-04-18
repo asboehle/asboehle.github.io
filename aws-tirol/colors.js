3dccdbc4b8bda3e6f4ddf102db27833b732f668b
asboehle <Ann-Sophie.Boehle@student.uibk.ac.at>
2021-04-18 17:58:23 +0200

    Korrektur

--- aws-tirol/colors.js
+++ aws-tirol/colors.js
@@ -28,38 +28,98 @@ const COLORS = {
         max: 999,
         col: "#ff3232"
     }],
+
+
     temperature: [{
+        min: -25,
+        max: -999,
+        col: "#9f80ff"
+    }, {
+        min: -25,
+        max: -20,
+        col: "#784cff"
+    }, {
+        min: -20,
+        max: -15,
+        col: "#0f5abe"
+    }, {
+        min: -15,
+        max: -10,
+        col: "#1380ff"
+    }, {
+        min: -10,
+        max: -5,
+        col: "#19cdff"
+    }, {
+        min: -5,
+        max: 0,
+        col: "#8fffff"
+    }, {
         min: 0,
         max: 5,
-        col: "#ffff64"
-    }, {
+        col: "#b0ffbc"
+    },{
         min: 5,
         max: 10,
-        col: "#c8ff64"
+        col: "#ffff73"
+    },{
+        min: 10,
+        max: 15,
+        col: "#ffbe7d"
+    },{
+        min: 15,
+        max: 20,
+        col: "#ff9b41"
+    },{
+        min: 20,
+        max: 25,
+        col: "#ff5a41"
+    },{
+        min: 25,
+        max: 30,
+        col: "#ff1e23"
+    },{
+        min: 30,
+        max: 999,
+        col: "#fa3c96"
+    }],
+
+
+    snowheight: [{
+        min: 0,
+        max: 1,
+        col: "#fff"
+    }, {
+        min: 1,
+        max: 10,
+        col: "#ffffb2"
     }, {
         min: 10,
-        max: 20,
-        col: "#96ff96"
+        max: 25,
+        col: "#b0ffbc"
     }, {
-        min: 20,
-        max: 40,
-        col: "#32c8ff"
+        min: 25,
+        max: 50,
+        col: "#8cffff"
     }, {
-        min: 40,
-        max: 60,
-        col: "#6496ff"
+        min: 50,
+        max: 100,
+        col: "#19cdff"
     }, {
-        min: 60,
-        max: 80,
-        col: "#9664ff"
+        min: 100,
+        max: 200,
+        col: "#1982ff"
     }, {
-        min: 80,
+        min: 200,
+        max: 300,
+        col: "#0f5abe"
+    },{
+        min: 300,
+        max: 400,
+        col: "#784bff"
+    },{
+        min: 400,
         max: 999,
-        col: "#ff3232"
-    }
-
-    ],
-    snowheight: [
-
-    ]
+        col: "#cd0feb"
+    }]
 };
