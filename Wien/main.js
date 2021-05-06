2dca4f761817e097c7c9b30715661a123d5f738d
asboehle <Ann-Sophie.Boehle@student.uibk.ac.at>
2021-05-06 15:50:04 +0200

    fullscreen hinzu

--- Wien/main.js
+++ Wien/main.js
@@ -22,7 +22,9 @@ let overlays = {
 };
 
 // Karte initialisieren und auf Wiens Wikipedia Koordinate blicken
+//fullscreen einbauen
 let map = L.map("map", {
+    fullscreenControl: true,
     center: [48.208333, 16.373056],
     zoom: 13,
     layers: [
